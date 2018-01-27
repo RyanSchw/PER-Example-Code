bfd76d2157df3fe89ab554519f72708bcca82f84
Ryan Schwartz <schwar95@purdue.edu>
2018-01-26 19:14:48 -0500

    PWM-Input

---
+++ PWM-Generation-STM32L432/Debug/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.d
@@ -0,0 +1,92 @@
+Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.o: \
+ ../Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Inc/stm32l4xx_hal_conf.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Inc/main.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/core_cmInstr.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/core_cmFunc.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/core_cmSimd.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h \
+ /Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Inc/stm32l4xx_hal_conf.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Inc/main.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/core_cm4.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h:
+
+/Users/ryan/Documents/BitBucket/PER-Example-Code/PWM-Generation-STM32L432/PWM-Generation/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h:
---
+++ PWM-Input-STM32F407/README.md
@@ -0,0 +1,3 @@
+Uses interrupt to increment counter that's using RTOS functions w/ reset of 1s
+Board: STM32F407G-DISC1
+Pins: GPIO-Int PA5
